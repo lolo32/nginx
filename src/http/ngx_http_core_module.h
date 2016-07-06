0c2d88d7091e8723775a028f030bfdd87480b504
Laurent B. (Lolo_32) <lol.b@free.fr>
2016-07-06 17:01:11 +0200

    Added patch for HTTP/2 and SPDY simultaneously
    Patch applied to add SPDY support with already added HTTP2 connection.  These
    need to be enabled during compilation, with these options:
      --with-http_spdy_module
      --with-http_v2_module
    Both can be enabled at same time, or only one.
    
    Patch based on Cloudflare patch, for nginx 1.9.7:
      https://github.com/cloudflare/sslconfig/blob/master/patches/nginx__http2_spdy.patch

--- src/http/ngx_http_core_module.h
+++ src/http/ngx_http_core_module.h
@@ -71,6 +71,9 @@ typedef struct {
 #if (NGX_HTTP_V2)
     unsigned                   http2:1;
 #endif
+#if (NGX_HTTP_SPDY)
+    unsigned                   spdy:1;
+#endif
 #if (NGX_HAVE_INET6 && defined IPV6_V6ONLY)
     unsigned                   ipv6only:1;
 #endif
@@ -239,6 +242,9 @@ struct ngx_http_addr_conf_s {
 #endif
 #if (NGX_HTTP_V2)
     unsigned                   http2:1;
+#endif
+#if (NGX_HTTP_SPDY)
+    unsigned                   spdy:1;
 #endif
     unsigned                   proxy_protocol:1;
 };
