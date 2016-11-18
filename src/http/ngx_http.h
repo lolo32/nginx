95806dae140db4c067c570e6848a3948685d8637
Laurent B. (Lolo_32) <lol.b@free.fr>
2016-11-18 19:18:49 +0100

    Added patch for HTTP/2 and SPDY simultaneously
    Patch applied to add SPDY support with already added HTTP2 connection.  These
    need to be enabled during compilation, with these options:
      --with-http_spdy_module
      --with-http_v2_module
    Both can be enabled at same time, or only one.
    
    Patch based on Cloudflare patch, for nginx 1.9.7:
      https://github.com/cloudflare/sslconfig/blob/master/patches/nginx__http2_spdy.patch

--- src/http/ngx_http.h
+++ src/http/ngx_http.h
@@ -20,6 +20,9 @@ typedef struct ngx_http_file_cache_s  ngx_http_file_cache_t;
 typedef struct ngx_http_log_ctx_s     ngx_http_log_ctx_t;
 typedef struct ngx_http_chunked_s     ngx_http_chunked_t;
 typedef struct ngx_http_v2_stream_s   ngx_http_v2_stream_t;
+#if (NGX_HTTP_SPDY)
+typedef struct ngx_http_spdy_stream_s  ngx_http_spdy_stream_t;
+#endif
 
 typedef ngx_int_t (*ngx_http_header_handler_pt)(ngx_http_request_t *r,
     ngx_table_elt_t *h, ngx_uint_t offset);
@@ -38,6 +41,9 @@ typedef u_char *(*ngx_http_log_handler_pt)(ngx_http_request_t *r,
 #if (NGX_HTTP_V2)
 #include <ngx_http_v2.h>
 #endif
+#if (NGX_HTTP_SPDY)
+#include <ngx_http_spdy.h>
+#endif
 #if (NGX_HTTP_CACHE)
 #include <ngx_http_cache.h>
 #endif
@@ -46,6 +52,9 @@ typedef u_char *(*ngx_http_log_handler_pt)(ngx_http_request_t *r,
 #endif
 #if (NGX_HTTP_SSL)
 #include <ngx_http_ssl_module.h>
+#endif
+#if (NGX_HTTP_SPDY)
+typedef struct ngx_http_spdy_stream_s  ngx_http_spdy_stream_t;
 #endif
 
 
